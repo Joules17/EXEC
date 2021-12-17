6f752f20ae8b6c1b4aaff50e4f7590bccf61f071
Joules <52895213+Joules17@users.noreply.github.com>
2021-12-17 12:29:46 -0500

    Add files via upload

--- leercadena_main.c
+++ leercadena_main.c
@@ -1,50 +1,34 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include "leercadena.h"
-
 
 int main(int argc, char *argv[]) {
-    char **vector; 
-
-    if (argc == 1){
+    int i; 
+    char* vec[argc]; 
+    if (argc == 1){ // Se verifica que el usuario no haya ingresado nada, de ser el caso, le pedira el programa que desea se ejecute. 
         printf("Indique el programa que desea se ejecute \n"); 
         exit(1);
         
-    } else {       
-        convierte_string(argv); 
+    } else { 
         pid_t pid;  
-        if (strcmp ('exit', argv[1]) ==  0) {
-            exit(1); 
-        }
         pid = fork(); 
         if (pid < 0) {
             perror("No se pudo crear un proceso \n"); 
             exit(1); 
         } else if (pid == 0) {
-            convierte_char(argv); 
-            vector = de_cadena_a_vector(comando); 
-            execvp(vector[0], vector); 
-            perror("Return from execlp() not expected"); 
-            exit(EXIT_FAILURE); 
+          argv[argc] = NULL; 
+          for (i = 0; i < argc; i++) {
+            vec[i] = argv[i+1]; 
+          }          
+          execvp(vec[0], vec); 
+          perror("Return from execlp() not expected"); 
+          exit(EXIT_FAILURE); 
         } else {
             wait(NULL); 
         }
-         
     }
     return 0; 
 }
-
-
-char* convierte_char(char *argv[]) {
-    int i; 
-    char var[25]; 
-    for (i = 1; i < sizeof(argv); i++) {
-        var[i] = argv[i]; 
-    }
-}
-
-
